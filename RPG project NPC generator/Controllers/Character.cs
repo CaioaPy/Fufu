07249cc2b00b462fe658729e2428d4d6dbc6ba0e
RemoteBeast <caioabner@ddsinfo.com.br>
2025-01-09 10:30:26 -0300

    New Methods for character and history.

---
+++ RPG project NPC generator/Controllers/Character.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class Character : Controller
+{
+    [HttpGet]
+    public IActionResult GetCharacter()
+    {
+        var name = "test";
+        var age = 10;
+        var history = "history placeholder";
+        var character = new
+        {
+            Name = @name,
+            Age = @age,
+            History = @history
+        };
+        return Ok(character);
+    }
+}
---
+++ RPG project NPC generator/Controllers/History.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class History : Controller
+{
+
+    [HttpPost]
+    public IActionResult PostHistory()
+    {
+        var validation = "We've received your history! Thank you!";
+        return Ok(validation);
+    }
+}
