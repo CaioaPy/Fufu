b1a82e848de94d5bc90ec61c3bc90ce515f94eac
yatomikazuchi <caioevilabner@gmail.com>
2025-01-08 23:12:24 -0300

    Adicionar arquivos de projeto.

---
+++ RPG project NPC generator/Controllers/PersonagemController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PersonagemController : Controller
+{
+    [HttpGet]
+    public IActionResult GetProdutos()
+    {
+        var personagem = "test";
+        return Ok(personagem);
+    }
+}
