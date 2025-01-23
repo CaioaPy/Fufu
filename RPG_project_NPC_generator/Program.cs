a008baa47695f40210f806add214d8871ac10415
yatomikazuchi <caioevilabner@gmail.com>
2025-01-22 21:13:27 -0300

    removed docker, updated main backend folder name

--- RPG_project_NPC_generator/Program.cs
+++ RPG_project_NPC_generator/Program.cs
@@ -3,6 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()   // Allows requests from any origin
+              .AllowAnyMethod()   // Allows any HTTP method (GET, POST, etc.)
+              .AllowAnyHeader();  // Allows any headers
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +29,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Use the CORS policy
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
